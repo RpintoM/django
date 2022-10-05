67d8636c5a7fb2aa2086df831642402ca52e971e
RpintoM <rafaelepinto@uniguajira.edu.co>
2022-10-04 21:19:06 -0500

    tercer parrafo

---
+++ tienda/Apps/productos/models.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from Apps.clientes.models import Cliente
+from django.db import models
+
+#from clases.tienda.Apps.provedores.models import Provedore
+# Create your models here.
+
+
+class Producto(models.Model):
+    descricion = models.CharField(max_length=50, verbose_name="Describir producto")
+    precio = models.FloatField(verbose_name="Precio")
+    numeroExistencia = models.IntegerField(verbose_name="Cantidad")
+   
+
+    def __str__(self):
+        return self.descricion
+
+
+      
+class Comprar(models.Model):
+    fechaCompra = models.DateTimeField(auto_now=True, verbose_name="Fecha de Compra")
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, verbose_name="Cliente")
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE, verbose_name="Producto")
+
+    #def __str__(self):
+        #return self.nombre
+
+    
